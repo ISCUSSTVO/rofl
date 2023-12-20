b83bc5a74f51fa7578e6e5d545c1adf632a6f167
ISCUSSTVO <nikishka228@list.ru>
2023-12-20 13:50:50 +1000

    5647

--- data_base/database.py
+++ data_base/database.py
@@ -1,5 +1,5 @@
 import sqlite3 as sq
-from createbot import bot
+
 
 def sql_start():
     global base, cur
@@ -7,21 +7,21 @@ def sql_start():
     cur = base.cursor()
     if base:
         print('Data base connected: OK')
-    base.execute('CREATE TABLE IF NOT EXISTS list(name TEXT PRIMARY KEY, adres TEXT, number INT)')
+    base.execute("CREATE TABLE IF NOT EXISTS list(name TEXT PRIMARY KEY, adres TEXT, number TEXT)")
     base.commit()
 
 async def sql_add_command(state):
     async with state.proxy() as data:
-        cur.execute('INSERT INTO list (name, adres, number) VALUES(?, ?, ?)', tuple(data.values()))
+        cur.execute("INSERT INTO list VALUES(?, ?, ?)", tuple(data.values()))
         base.commit()
 
 async def sql_read(message):
     for ret in cur.execute('SELECT * FROM list').fetchall():
-        await bot.send_message(message.from_user.id, f'{ret[1]}\nИмя: {ret[2]}\nВаш адрес {ret[3]}\nНомер телефона')
+        await message.answer(f'Имя: {ret[0]}\nВаш адрес: {ret[1]}\nНомер телефона: {ret[2]}')
 
 async def sql_read2():
     return cur.execute('SELECT * FROM list').fetchall()
 
 async def sql_delete_command(data):
-    cur.execute('DELETE FROM list WHERE name == ?', (data,))
+    cur.execute('DELETE FROM list WHERE name == ?', (data))
     base.commit()
--- handlers/client.py
+++ handlers/client.py
@@ -1,74 +1,79 @@
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
+import sqlite3
 from aiogram import types, Dispatcher
-from createbot import dp, bot
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from keyboard import client_kb
-from keyboard import cjd
+
+import keyboard.client_kb
+from createbot import dp, bot
 from data_base import database
 from keyboard import Regestration_kb
-x = int(0)
-y = int(0)
-z = int(0)
+from keyboard import cjd
 
-bool(x)
-bool(y)
-bool(z)
+conn = sqlite3.connect('super_roflo.db')
+cursor = conn.cursor()
 
-class FSMClient(StatesGroup):
+ID = None
+
+class FSMRegestration(StatesGroup):
     name = State()
     adres = State()
     number = State()
-x = 1
+
+class FSMClient(StatesGroup):
+    name2 = State()
 
 @dp.message_handler(commands=['start', 'help'])
 async def command_start(message: types.message):
     await bot.send_message(message.from_user.id, 'Здарова', reply_markup=Regestration_kb.button_case_regestration)
     await message.delete()
 
-@dp.message_handler(commands=['list'])
+@dp.message_handler(commands=['Лист'])
 async def list(message: types.Message):
     await database.sql_read(message)
 
 @dp.message_handler(commands='Заказ_услуги', state=None)
 async def call_service(message: types.Message):
-    await bot.send_message(message.from_user.id, 'Выбери услугу', reply_markup=cjd.asd)
-    await message.delete()
+    if message.from_user.id == ID:
+        await message.answer('Выбери услугу', reply_markup=cjd.asd)
+        await message.delete()
+    else:
+        await message.answer('Иди зарегайся')
 
 @dp.message_handler(commands=['Информация_об_услугах'], state=None)
 async def info_service(message: types.Message):
-    await message.reply('Выберите услугу по которой хотите получить информацию', reply_markup=cjd.asd)
-    await message.delete()
+    if message.from_user.id == ID:
+        await message.answer('Выберите услугу по которой хотите получить информацию', reply_markup=cjd.asd)
+        await message.delete()
+    else:
+        await message.answer('Иди зарегайся')
 
 @dp.message_handler(commands='Регестрация', state=None)
 async def cm_start(message: types.Message):
-    await FSMClient.name.set()
+    await FSMRegestration.name.set()
     await message.reply('Введи своё имя')
 
-@dp.message_handler(state=FSMClient.name)
+@dp.message_handler(state=FSMRegestration.name)
 async def load_name(message: types.Message, state: FSMContext):
-    not bool(x)
     async with state.proxy() as data:
         data['name'] = message.text
-    await FSMClient.next()
+    await FSMRegestration.next()
     await message.reply('Теперь введи свой адрес')
-@dp.message_handler(content_types=['adres'], state=FSMClient.adres)
+@dp.message_handler(content_types=['adres'], state=FSMRegestration.adres)
 async def load_adres(message: types.Message, state: FSMContext):
-    not bool(y)
     async with state.proxy()as data:
         data['adres'] = message.text
-    await FSMClient.next()
+    await FSMRegestration.next()
     await message.reply('Тепрь введи свой номер телефона')
-@dp.message_handler(content_types=['number'], state=FSMClient.number)
+@dp.message_handler(content_types=['number'], state=FSMRegestration.number)
 async def load_number(message: types.Message, state: FSMContext):
-    not bool(z)
+    global ID
+    ID = message.from_user.id
     async with state.proxy() as data:
-        data['number'] = float(message.text)
-
-        await database.sql_add_command(state)
-        await state.finish()
+        data['number'] = message.text
+    await database.sql_add_command(state)
+    await state.finish()
 
 @dp.message_handler(state="*", commands='отмена')
 @dp.message_handler(Text(equals='отмена', ignore_case=True), state="*")
@@ -88,16 +93,16 @@ async def del_callback_run(callback_querry: types.CallbackQuery):
 async def delete_items(message: types.Message):
     read = await database.sql_read2()
     for ret in read:
-        await bot.send_message(message.from_user.id, f'{ret[0]}\nИмя {ret[1]}\nАдрес {ret[2]}\nНомер')
+        await bot.send_message(message.from_user.id, f'Имя: {ret[0]}\nВаш адрес: {ret[1]}\nНомер телефона: {ret[2]}')
         await bot.send_message(message.from_user.id, text='^^^', reply_markup=InlineKeyboardMarkup().\
                                 add(InlineKeyboardButton(f'Удалить {ret[0]}', callback_data=f'del {ret[0]}')))
 
 @dp.message_handler(commands=['client'])
 async def client(message: types.Message):
-    await bot.send_message(message.from_user.id, 'Выберете, что вам нужно', reply_markup=client_kb.prikol)
-    await message.delete()
-
-
+    if message.from_user.id == ID:
+        await message.reply('Что нужно', reply_markup=keyboard.client_kb.prikol)
+    else:
+        await message.answer('Ди зарегайся')
 
 def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(client, commands=['client'])
@@ -105,10 +110,10 @@ def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(info_service, commands=['Информация_об_услугах'])
     dp.register_message_handler(command_start, commands=['start', 'help'])
     dp.register_message_handler(cm_start, commands=['Регестрация'], state=None)
-    dp.register_message_handler(list, commands=['list'])
-    dp.register_message_handler(load_name, state=FSMClient.name)
-    dp.register_message_handler(load_adres, state=FSMClient.adres)
-    dp.register_message_handler(load_number, state=FSMClient.number)
+    dp.register_message_handler(list, commands=['Лист'])
+    dp.register_message_handler(load_name, state=FSMRegestration.name)
+    dp.register_message_handler(load_adres, state=FSMRegestration.adres)
+    dp.register_message_handler(load_number, state=FSMRegestration.number)
     dp.message_handler(cancel_handler, state="*", commands='отмена')
     dp.message_handler(cancel_handler, Text(equals='отмена', ignore_case=True), state="*")
 
